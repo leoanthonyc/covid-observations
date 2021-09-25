aa168e2794fbdb7687f5f6b2e952cdb40f3c62ab
Leo Catane <leoanthony.catane@protonmail.com>
2021-09-25 11:33:59 +0800

    Add html view, allow json format of result

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,4 +1,21 @@
 class ObservationsController < ApplicationController
   def confirmed
+    rows = CovidObservation.order(confirmed: :desc)
+
+    limit = params[:max_results]
+    rows = rows.limit(limit) if limit.present?
+
+    observation_date = params[:observation_date]
+    rows = rows.where(observation_date: observation_date) if observation_date.present?
+
+    @result = { 
+      observation_date: observation_date,
+      countries: rows.map { |row| row.as_json(only: %i(country confirmed deaths recovered)) }
+    }
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @result }
+    end
   end
 end
--- app/models/covid_observation.rb
+++ app/models/covid_observation.rb
@@ -1,2 +1,3 @@
 class CovidObservation < ApplicationRecord
+
 end
